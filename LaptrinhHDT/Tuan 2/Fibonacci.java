8a7b7dc4294530bf06f1483a9a6dc4a007969e22
TrinhDiep <20010935@st.phenikaa-uni.edu.vn>
2022-03-05 21:48:00 +0700

    bai 1 bai 2 tuan 2

---
+++ LaptrinhHDT/Tuan 2/Fibonacci.java
@@ -0,0 +1,20 @@
+package fibonacci;
+public class Fibonacci {
+   public static void main(String[] args) {
+       int a1 = 0;
+       int a2 = 1;
+       int a3 = 1;
+       int a4 = 2;
+       int a5 = 3;
+       int a6 = 5;
+       int a7 = 8;
+       int a8 = 13;
+       int a9 = 21;
+       int a10 = 34;
+       int a11 = 55;
+       int a12 = 89;
+       int a = a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10 + a11 + a12;
+       System.out.println("Tong day so la: "+ a);
+    }
+    
+}
