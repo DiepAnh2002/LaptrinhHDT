007c0653406bfb7068388eaf807fae32150da16f
TrinhDiep <20010935@st.phenikaa-uni.edu.vn>
2022-05-16 09:53:04 +0700

    thuchanh3

--- LaptrinhHDT/thuchanh3/DigitalWatch.java
+++ LaptrinhHDT/thuchanh3/DigitalWatch.java
@@ -1,50 +1,52 @@
-package javathread;
-
 import javax.swing.*;
 import java.text.*;
 import java.util.*;
 
-public class DigitalWatch  {
+public class DigitalWatch implements Runnable {
+    JFrame f;
+    JFrame f1;
+    Thread t = null;
+    String timeString = "";
+    String timeString1 = "";
+    JButton b;
+    JButton l;
+    DigitalWatch() {
+        f = new JFrame();
+// f1 = new JFrame();
+t = new Thread(this);
+t.start();
 
-   JFrame f;
-   Thread t = null;
-   
-   String timeString = "";
-   JButton b;
-   
-   DigitalWatch() {
-       f = new JFrame();
-       
-       t = new Thread((Runnable) this);
-       t.start();
-       
-       b = new JButton();
-       b.setBounds(100, 100, 100, 50);
-       
-       f.add(b);
-       f.setSize(300, 400);
-       f.setLayout(null);
-       f.setVisible(true);
-   }
-    public static void run() {
+b = new JButton();
+b.setBounds(100, 100, 100, 50);
+l = new JButton();
+l.setBounds(300, 200, 200, 222);
+
+f.add(b);
+f.setSize(300, 400);
+f.setLayout(null);
+f.setVisible(true);
+    }
+    public void run() {
         try {
             while (true) {
                 SimpleDateFormat formatter = new SimpleDateFormat("hh:mm:ss");
-                timeString = formatter.format(new Date());
-                
-                printTime();
-                
-                Thread.sleep(1000);
+// Date date = cal.getTime();
+timeString = formatter.format(new Date());
+printTime();
+SimpleDateFormat formatter1 = new SimpleDateFormat("hh:mm:ss");
+timeString1 = formatter1.format(new Date());
+t.sleep(1000);
+// interval given in milliseconds
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
         }
     }
-    public void printTime () {
-        b.setText (timeString) {
+
+    public void printTime() {
+        b.setText(timeString);
+// l.setText(timeString1);
     }
-        
-    public static void main(String[] args){
-            new JavaThread();
+    public static void main(String[] args) {
+        new DigitalWatch();
     }
 }
